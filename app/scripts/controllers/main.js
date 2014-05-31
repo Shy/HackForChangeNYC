046580c6f6407d60520e3769cc4a0a7399a45879
Matt Condon <MattCMultimedia@gmail.com>
2014-05-31 17:50:42 -0400

    move login

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,25 +5,4 @@ angular.module('communiTaskApp')
     var taskRef = new Firebase('https://torid-fire-4640.firebaseio.com/task');
 
     $scope.tasks = $firebase(taskRef);
-
-
-    var auth = new FirebaseSimpleLogin(taskRef, function(error, user) {
-        if (error) {
-            // an error occurred while attempting login
-            console.log(error);
-        } else if (user) {
-            // user authenticated with Firebase
-            console.log('User ID: ' + user.uid);
-        } else {
-            // user is logged out
-        }
-    });
-
-    $scope.login = function() {
-        auth.login('facebook', {
-            rememberMe: true,
-            scope: 'email'
-        });
-    };
-
 });
--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -5,7 +5,29 @@ angular.module('communiTaskApp')
     return {
       templateUrl: 'views/navbar.html',
       restrict: 'AE',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink($scope, element, attrs) {
+
+        var taskRef = new Firebase('https://torid-fire-4640.firebaseio.com/task');
+
+        var auth = new FirebaseSimpleLogin(taskRef, function(error, user) {
+            if (error) {
+                // an error occurred while attempting login
+                console.log(error);
+            } else if (user) {
+                $scope.user = user;
+                // user authenticated with Firebase
+                console.log('User ID: ' + user.uid);
+            } else {
+                // user is logged out
+            }
+        });
+
+        $scope.login = function() {
+            auth.login('facebook', {
+                rememberMe: true,
+                scope: 'email'
+            });
+        };
 
       }
     };
