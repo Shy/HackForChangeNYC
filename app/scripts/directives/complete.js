92317d95048e50b48fd484d78fcc9071134118a9
Matt Condon <MattCMultimedia@gmail.com>
2014-05-31 17:26:22 -0400

    add complete

--- app/scripts/directives/complete.js
+++ app/scripts/directives/complete.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('communiTaskApp')
-.directive('complete', function () {
+.directive('complete', function ($location, $timeout) {
     return {
         templateUrl: 'views/complete.html',
         restrict: 'AE',
         link: function postLink(scope, element, attrs) {
-
+            scope.submitComplete = function() {
+                $timeout(function() {
+                    $location.path('/');
+                }, 500);
+            };
         }
     };
 });
